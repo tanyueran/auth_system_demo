9dba6f708f33632ce1f2685e20e468c97e966104
tanyueran <txloveu@163.com>
2020-06-26 21:51:54 +0800

    添加菜单权限判断

--- front-end-code/auth_system/src/components/MNav.js
+++ front-end-code/auth_system/src/components/MNav.js
@@ -4,6 +4,7 @@
  * @Description:
  */
 import React from 'react';
+import {connect} from 'react-redux';
 import {withRouter} from 'react-router'
 import {Menu} from "antd";
 import {
@@ -21,7 +22,20 @@ class MNav extends React.Component {
     this.props.history.push(val.key);
   };
 
+  static stateToProps(state) {
+    return {
+      user: state.userReducer
+    }
+  }
+
   render() {
+    let menus = [];
+    this.props.user.userInfo.data.roles && this.props.user.userInfo.data.roles.forEach(item => {
+      item.menus.forEach(item2 => {
+        menus[item2.menuCode] = item2;
+      })
+    });
+
     return <Menu mode="inline" onClick={this.clickHandler} selectedKeys={this.props.location.pathname}>
       <Menu.Item key="/home/index">
         <HomeOutlined/>
@@ -34,26 +48,38 @@ class MNav extends React.Component {
         </Menu.Item>
       </Menu.SubMenu>
       <Menu.SubMenu key="sub2" icon={<AppstoreOutlined/>} title="系统管理">
-        <Menu.Item key="/home/system_manager/user_manager">
-          <TeamOutlined/>
-          <span>用户管理</span>
-        </Menu.Item>
-        <Menu.Item key="/home/system_manager/role_manager">
-          <ClusterOutlined/>
-          <span>角色管理</span>
-        </Menu.Item>
-        <Menu.Item key="/home/system_manager/menu_manager">
-          <MenuOutlined/>
-          <span>菜单管理</span>
-        </Menu.Item>
-        <Menu.Item key="/home/system_manager/btn_manager">
-          <UnlockOutlined/>
-          <span>按钮管理</span>
-        </Menu.Item>
+        {
+          menus['userManager'] !== undefined
+          && <Menu.Item key="/home/system_manager/user_manager">
+            <TeamOutlined/>
+            <span>用户管理</span>
+          </Menu.Item>
+        }
+        {
+          menus['roleManager'] !== undefined
+          && <Menu.Item key="/home/system_manager/role_manager">
+            <ClusterOutlined/>
+            <span>角色管理</span>
+          </Menu.Item>
+        }
+        {
+          menus['menuManager'] !== undefined
+          && <Menu.Item key="/home/system_manager/menu_manager">
+            <MenuOutlined/>
+            <span>菜单管理</span>
+          </Menu.Item>
+        }
+        {
+          menus['buttonManager'] !== undefined
+          && <Menu.Item key="/home/system_manager/btn_manager">
+            <UnlockOutlined/>
+            <span>按钮管理</span>
+          </Menu.Item>
+        }
       </Menu.SubMenu>
     </Menu>
   }
 
 }
 
-export default withRouter(MNav);
+export default connect(MNav.stateToProps)(withRouter(MNav));
